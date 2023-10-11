67e743d6e0c0fc71b529ed4a8a513d7372797777
sanjeewna <sanjeewnadevi93@gmail.com>
2023-10-11 18:29:55 +0530

    Solved lab 8 week 2

--- app.js
+++ app.js
@@ -24,6 +24,66 @@ app.get("/api/recipes", (req, res) => {
       name: req.body.name,
       ingredients: req.body.ingredients,
     };
+
+    app.post("/api/recipes", (req, res) => {
+      // ... (existing code)
+    
+      // Your code to add a new recipe here
+    });
+
+    app.post("/api/recipes", (req, res) => {
+      const newRecipe = {
+        id: recipes.length + 1,
+        name: req.body.name,
+        ingredients: req.body.ingredients,
+      };
+    
+      recipes.push(newRecipe);
+      res.status(201).json(newRecipe);
+    });
+
+    app.put("/api/recipes/:id", (req, res) => {
+      // ... (existing code)
+    
+      // Your code to update a recipe here
+    });
+
+    app.put("/api/recipes/:id", (req, res) => {
+      const recipeId = parseInt(req.params.id);
+      const updatedRecipe = req.body;
+    
+      // Find the index of the recipe with the given ID
+      const recipeIndex = recipes.findIndex((recipe) => recipe.id === recipeId);
+    
+      if (recipeIndex !== -1) {
+        // Update the recipe with the new data
+        recipes[recipeIndex] = { id: recipeId, ...updatedRecipe };
+        res.json(recipes[recipeIndex]);
+      } else {
+        res.status(404).json({ message: "Recipe not found" });
+      }
+    });
+
+    app.delete("/api/recipes/:id", (req, res) => {
+      // ... (existing code)
+    
+      // Your code to delete a recipe here
+    });
+
+    app.delete("/api/recipes/:id", (req, res) => {
+      const recipeId = parseInt(req.params.id);
+    
+      // Find the index of the recipe with the given ID
+      const recipeIndex = recipes.findIndex((recipe) => recipe.id === recipeId);
+    
+      if (recipeIndex !== -1) {
+        // Remove the recipe from the array
+        recipes.splice(recipeIndex, 1);
+        res.json({ message: "Recipe deleted" });
+      } else {
+        res.status(404).json({ message: "Recipe not found" });
+      }
+    });
   
     recipes.push(newRecipe);
     res.status(201).json(newRecipe);
